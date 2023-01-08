0b66195806448aef876ca846ac46f6207b5a65fd
danielxf007 <daniel.higuita007@gmail.com>
2023-01-08 16:17:34 -0500

    added ASL operations

--- processor/cpu_operations.go
+++ processor/cpu_operations.go
@@ -7,11 +7,68 @@ func (addr* AddressBuffer) Increment(value byte) uint16 {
 	return uint16(carry)
 }
 
+//Arithmetic
+
+//Shift Left
+func updateFlagsASL(cpu* CPU, bit byte, result byte) {
+	cpu.P.C = bit
+	cpu.P.N = GetNBit(result, 7)
+	if result == 0x00 {
+		cpu.P.Z = 1
+	}else {
+		cpu.P.Z = 0
+	}
+}
+
+func ExecuteASLA(cpu* CPU) {
+	bit := GetNBit(cpu.A.Value, 7)
+	cpu.A.Value <<= 1
+	updateFlagsASL(cpu, bit, cpu.A.Value)
+}
+
+func ExecuteASLMEM(cpu* CPU) {
+	value := cpu.Mapper.Read(cpu.Addr.ADH, cpu.Addr.ADL)
+	bit := GetNBit(value, 7)
+	value <<= 1
+	updateFlagsASL(cpu, bit, value)
+	cpu.Mapper.Write(cpu.Addr.ADH, cpu.Addr.ADL, value)
+	cpu.PC.Increment(1)
+}
+
+func ASLA(cpu* CPU) uint16 {
+	ExecuteASLA(cpu)
+	return 1
+}
+
+func ASLZeroPage(cpu* CPU) uint16 {
+  GetZeroPageAddr(cpu)
+  ExecuteASLMEM(cpu)
+	return 5
+}
+
+func ASLZeroPageX(cpu* CPU) uint16 {
+	GetZeroPageXAddr(cpu)
+  ExecuteASLMEM(cpu)
+	return 6
+}
+
+func ASLAbsolute(cpu* CPU) uint16 {
+	GetAbsoluteAddr(cpu)
+	ExecuteASLMEM(cpu)
+	return 6
+}
+
+func ASLAbsoluteX(cpu* CPU) uint16 {
+	GetAbsoluteXAddr(cpu)
+  ExecuteASLMEM(cpu)
+	return 7
+}
+
 //Boolean
 
 //AND
 func updateFlagsAND(cpu* CPU, result byte) {
-	cpu.P.N = (result & 0x80) >> 7
+	cpu.P.N = GetNBit(result, 7)
 	if result == 0x00 {
 		cpu.P.Z = 1
 	}else {
@@ -75,7 +132,7 @@ func ANDIndirectY(cpu* CPU) uint16 {
 
 //ORA
 func updateFlagsORA(cpu* CPU, result byte) {
-	cpu.P.N = (result & 0x80) >> 7
+	cpu.P.N = GetNBit(result, 7)
 	if result == 0x00 {
 		cpu.P.Z = 1
 	}else {
