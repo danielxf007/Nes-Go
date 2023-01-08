11748f904549f0238886dd60650a6a6ce067f892
danielxf007 <daniel.higuita007@gmail.com>
2023-01-07 19:02:07 -0500

    added AND

--- processor/cpu_operations.go
+++ processor/cpu_operations.go
@@ -1,22 +1,75 @@
 package processor
 
+func (addr* AddressBuffer) Increment(value byte) uint16 {
+	carry := byte((uint16(addr.ADL) + uint16(value)) >> 8)
+	addr.ADL += value
+	addr.ADH += carry
+	return uint16(carry)
+}
+
 //Boolean
 
-func updateFlagsAND(cpu* CPU) {
-	cpu.P.N = (cpu.A.Value & 0x80) >> 7
-	if cpu.A.Value == 0x00 {
+func updateFlagsAND(cpu* CPU, result byte) {
+	cpu.P.N = (result & 0x80) >> 7
+	if result == 0x00 {
 		cpu.P.Z = 1
 	}else {
 		cpu.P.Z = 0
 	}
 }
 
-func ANDImmediate(cpu* CPU) uint16 {
-	value := cpu.Mapper.Read(cpu.PC.ADH, cpu.PC.ADL)
+func ExecuteAnd(cpu* CPU) {
+	value := cpu.Mapper.Read(cpu.Addr.ADH, cpu.Addr.ADL)
 	cpu.A.Value &= value
-	updateFlagsAND(cpu)
+	updateFlagsAND(cpu, cpu.A.Value)
 	cpu.PC.Increment(1)
+}
+
+func ANDImmediate(cpu* CPU) uint16 {
+  ExecuteAnd(cpu)
 	return 2
+}
+
+func ANDZeroPage(cpu* CPU) uint16 {
+  GetZeroPageAddr(cpu)
+  ExecuteAnd(cpu)
+	return 2
+}
+
+func ANDZeroPageX(cpu* CPU) uint16 {
+	GetZeroPageXAddr(cpu)
+  ExecuteAnd(cpu)
+	return 2
+}
+
+func ANDAbsolute(cpu* CPU) uint16 {
+	GetAbsoluteAddr(cpu)
+  ExecuteAnd(cpu)
+	return 4
+}
+
+func ANDAbsoluteX(cpu* CPU) uint16 {
+	adjusted := GetAbsoluteXAddr(cpu)
+  ExecuteAnd(cpu)
+	return 4 + adjusted
+}
+
+func ANDAbsoluteY(cpu* CPU) uint16 {
+	adjusted := GetAbsoluteYAddr(cpu)
+  ExecuteAnd(cpu)
+	return 4 + adjusted
+}
+
+func ANDIndirectX(cpu* CPU) uint16 {
+	GetIndirectXAddr(cpu)
+  ExecuteAnd(cpu)
+	return 6
+}
+
+func ANDIndirectY(cpu* CPU) uint16 {
+	adjusted := GetIndirectYAddr(cpu)
+  ExecuteAnd(cpu)
+	return 5 + adjusted
 }
 
 func (cpu* CPU) Execute(n_cycles uint16) {
